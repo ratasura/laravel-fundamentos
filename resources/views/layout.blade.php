7217d45beadb521e702377c5ede885aada8a7756
palmitas2015 <tiburcio2409@gmail.com>
2019-09-23 16:31:03 -0500

    comiit hasta que se arreglo el js del dropdown

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,88 +1,63 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet"  href="/css/app.css">
-    <style >
-
-   </style>
-    <title>Mi sitio</title>
-  </head>
-  <body>
-    <div class="container">
-
-    
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet"  href="/css/app.css">
+  <title>Mi sitio</title>
+</head>
+<body>
+  <div class="container">
     <header>
-      {{-- <h1>{{request()->is('/') ? 'Estas en el home' : 'No estas en el home'}}</h1> --}}
       <?php  function activeMenu($url){
         return request()->is($url) ? 'active' : '';
       } ?>
-
-
-      {{-- NAVBAR INICIO  --}}
-      <nav id="barra" class="navbar navbar-expand-lg navbar-light bg-light">
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class=" nav navbar-nav ">
-                      <li class="nav-item">
+      <nav id="barra1" class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div id="my-nav" class="collapse navbar-collapse">
+          <ul class="navbar-nav mr-auto">
+						<li class="nav-item">
                         <a class="nav-link " href="{{route('home') }}" tabindex="-1" aria-disabled="true">Inicio</a>
                       </li>
-                      <li class="nav-item active">
+                      <li class="nav-item ">
                         <a class="nav-link" href="{{route('saludos','Luis Mario') }}">Saludo </a>
                       </li>
                       <li class="nav-item">
                         <a class="nav-link" href="{{route('mensajes.create')}}">Contactos</a>
                       </li>
-
-                        @if (auth()->check())
-                      <li><a class="nav-link" href="/logout">Cerrar Sesion de {{auth()->user()->name}} </a></li>
-                          <li><a class="nav-link" href="{{ route('mensajes.index') }}">Mensajes</a></li>
-                        @endif
+            @if (auth()->check())
+            <li><a class="nav-link" href="{{ route('mensajes.index') }}">Mensajes</a></li>
+            @endif
+           </ul>
+           <ul class="nav navbar-nav ml-auto">
                         @if (auth()->guest())
-                        <a class="nav-link"  href="/login">Login</a>
-                        @endif
+                          <li><a class="nav-link" href="/login">Login</a></li> 
+                        @else
+                          <li><a class="nav-link" href="/logout">Cerrar Sesion de {{auth()->user()->name}} </a></li>
+                          
+                        @endif 
 
-                      <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown
-                      </a>
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                      </div>
-                    </li>
-
-              </ul>
-
-          </div>
-</nav>
-
-    {{--  NAVBAR FINAL --}}
-      {{-- <nav>
-        <a class="{{activeMenu('/')}}"
-            href="{{ route('home') }}">Inicio</a>
-        <a class="{{activeMenu('saludos*')}}"
-            href="{{ route('saludos','Luis Mario') }}">Saludo</a>
-        <a class="{{activeMenu('mensajes*')}}"
-            href="{{ route('mensajes.create') }}">Contacto</a>
-
-        @if (auth()->check())
-          <a href="/logout">Cerrar Sesion de {{auth()->user()->name}} </a>
-          <a class="{{activeMenu('Mensajes')}}"
-                  href="{{ route('mensajes.index') }}">Mensajes</a>
-        @endif
-        @if (auth()->guest())
-          <a class="{{activeMenu('login')}}"
-                          href="/login">Login</a>
-        @endif
-      </nav> --}}
+                        <li class="nav-item dropdown">
+                          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown
+                          </a>
+                          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                          </div>
+                        </li>
+           </ul>
+        </div>
+      </nav>
     </header>
   </div>
-    <div class="container">
-        @yield('contenido')
-      <footer>Coprygth {{date('Y')}}</footer>
-    </div>
-    
-  </body>
+  <div class="container">
+    @yield('contenido')
+  <footer>Coprygth {{date('Y')}}</footer>
+  <script src="js/all.js">
+  </script>
+</div>  
+</body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -2,3 +2,9 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+    //gulpfiles
+mix.scripts([
+	'node_modules/jquery/dist/jquery.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js'
+], 'public/js/all.js', 'node_modules');
